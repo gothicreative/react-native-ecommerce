2c389a89b8f3de2d416583ef9aaf223992034fb0
hafiz adem <huhuhudadada393@gmail.com>
2025-12-15 11:30:18 +0300

    call all in one

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,29 @@
 import express from 'express';
+import path from 'path';
+import { ENV } from './config/env.js';
 
-const app = express();
+
+
+const app = express() ;
+const __dirname = path.resolve();
 
 app.get('/api/health',(req,res)=>{
-    res.send('OK').json({message:'the first api is connected sucessfully'})
-});
+    res.status(200).json({message:'the first api is connected sucessfully'})
+})
+ 
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../admin/dist')));
+    app.get('/{*any}', (req, res) =>
+      res.sendFile(path.join(__dirname, '../admin', 'dist', 'index.html'))
+    );
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
 
-app.listen(3000, ()=> console.log("the first api is connected sucessfully"))
+app.listen(ENV.PORT, () => 
+   {
+    console.log(`Server is running on port ${ENV.PORT}`)
+   }    
+ )
