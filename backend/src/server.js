1810374dffcdf927b4300fff8901d29a33d331b6
hafiz adem <huhuhudadada393@gmail.com>
2025-12-18 15:08:28 +0300

    Syntax Error in Backend Server Route

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,28 +2,26 @@ import express from 'express';
 import path from 'path';
 import { ENV } from './config/env.js';
 
-
-
 const app = express() ;
 const __dirname = path.resolve();
 
 app.get('/api/health',(req,res)=>{
     res.status(200).json({message:'the first api is connected sucessfully'})
 })
- 
+
 if (ENV.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../admin/dist')));
-    app.get('/{*any}', (req, res) =>
-      res.sendFile(path.join(__dirname, '../admin', 'dist', 'index.html'))
+    app.get('*', (req, res) =>
+      res.sendFile(path.join(__dirname, '../admin/dist/index.html'))
     );
-  } else {
+} else {
     app.get('/', (req, res) => {
       res.send('API is running....');
     });
-  }
+}
 
 app.listen(ENV.PORT, () => 
    {
     console.log(`Server is running on port ${ENV.PORT}`)
    }    
- )
+)
