767502b0121f0ac2e1c471cbd7eacc4c2b060d7c
hafiz adem <huhuhudadada393@gmail.com>
2025-12-24 11:04:38 +0300

    start server method changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,9 +26,11 @@ if (ENV.NODE_ENV === 'production') {
     });
 }
 
-app.listen(ENV.PORT, () => 
-   {
-    console.log(`Server is running on port ${ENV.PORT}`)
-    connectDB()
-   }    
-)
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
+  });
+};
+
+startServer();
