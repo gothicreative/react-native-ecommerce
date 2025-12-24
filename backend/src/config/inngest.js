934a56649847d225bb924c0d2d5c7ad47f9a2ae7
hafiz adem <huhuhudadada393@gmail.com>
2025-12-24 17:47:51 +0300

    setup inngest with clerk

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,38 @@
+import {Inngest} from "inngest";
+import {connectDB} from "./db.js";
+import {User} from "../models/user.model.js";
+
+
+export const inngest = new Inngest({id: "ecommerce-App"});
+
+const syncUser = inngest.createFunction(
+    {id: "sync-user"},
+    {event: 'clerk/user.created'},
+    async ({event}) => {
+        await connectDB();
+        const {id, email_address, first_name, last_name, image_Url,} = event.data;
+        
+        const newUser = {
+            clerkId: id,
+            email: email_address[0]?.email_address,
+            name: `${first_name || ""} ${last_name || ""}` || 'User',
+            imageUrl: image_Url,
+            addresses: [],
+            wishlist: [],
+        };
+        const user = await User.create(newUser);
+        console.log(user);
+    }
+);
+
+const deleteUserFromDB = inngest.createFunction(
+    {id: "delete-user-from-db"},
+    {event: 'clerk/user.deleted'},
+    async ({event}) => {
+        await connectDB();
+        const user = await User.deleteOne({clerkId: event.data.id});
+        console.log(user);
+    }
+);
+
+export const functions = {syncUser, deleteUserFromDB};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import path from 'path';
 import { clerkMiddleware } from '@clerk/express'
-
+import {serve} from 'inngest/express'
+import { inngest, functions } from './config/inngest.js';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
 
 
 const app = express() ;
 const __dirname = path.resolve();
+app.use(express.json());
 
 app.use(clerkMiddleware())
+app.use('/api/inngest', serve({client: inngest, functions: [functions.syncUser, functions.deleteUserFromDB]}))
 
 app.get('/api/health',(req,res)=>{
     res.status(200).json({message:'the first api is connected sucessfully'})
